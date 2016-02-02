70162b7c81a7ad0d1527fcdcc490b218218985e5
Brian Riedmiller <RMiller517@gmail.com>
2016-02-02 00:37:34 -0500

    Initial commit

---
+++ app/src/androidTest/java/com/firebaseapp/dazzling_inferno_3455/mobappandroid/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.firebaseapp.dazzling_inferno_3455.mobappandroid;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
